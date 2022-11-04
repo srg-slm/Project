d42133f2d3839d97da4dc092bdceb5f6e5c2c574
slm <xslamx@email.com>
2022-11-04 17:08:30 +0300

    First commit

---
+++ test.py
@@ -0,0 +1,62 @@
+# a = True
+# b = False
+# print('a and b is', a and b)
+# print('a or b is', a or b)
+# print('not a is', not a)
+
+
+# def is_even(num):
+#     return num % 2 == 0
+
+# print(is_even(8))
+# print(is_even(7))
+
+
+# print(isinstance(3, int))
+# print(isinstance(3.5, float))
+# print(isinstance('Beegeek', str))
+# print(isinstance([1, 2, 3], list))
+# print(isinstance(True, bool))
+
+# print(type(3))
+# print(type(3.5))
+# print(type('Beegeek'))
+# print(type([1, 2, 3]))
+# print(type(True))
+
+
+# a = input()
+# print(isinstance(a, str))
+
+
+
+# def func(x, y):
+#     return x % y == 0
+# num1, num2 = int(input()), int(input())
+# if func(num1, num2):
+#     print('Делится')
+# else:
+#     print('Не делится')
+
+
+# var = None
+# if var is None:   # используем оператор is
+#   print('None')
+# else:
+#   print('Not None')
+
+# var = None
+# if var == None:   # используем оператор ==
+#   print('None')
+# else:
+#   print('Not None')
+
+
+# var = None
+# print(var is None)
+
+
+# def print_message():
+#     print('Я - Тимур,')
+#     print('король матана. ')
+# print_message()
---
+++ test2.py
@@ -0,0 +1,7 @@
+# numbers = [10, 20, 30, 40]
+# del numbers[0:4]
+# print(numbers)
+
+numbers = [1, 2, 3, 4, 5, 6, 7]
+new_numbers =  [2 * x for x in numbers]
+print(new_numbers)
